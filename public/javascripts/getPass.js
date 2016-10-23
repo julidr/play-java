369326311f39fbc98d6af749ebbe3ec8a41bc6ec
julidr <juli.milena@hotmail.com>
2016-10-22 21:05:47 -0500

    Se agrego el archivo js que se olvido

--- public/javascripts/getPass.js
+++ public/javascripts/getPass.js
@@ -1,7 +1,33 @@
-$(function() {
-  return $.get("http://localhost:9000/passwordsList?plataforma=Twitter", function(passwords) {
-    return $.each(passwords, function(plataforma, password) {
-      return $('#passwords').append($("<td>").text(password));
+function busqueda(){
+	console.log("me dieron click");
+	$(function() {
+	var valorABuscar=$("#plataformaBusc").val().toLowerCase();
+  console.log(valorABuscar);
+  return $.get("/passwordsList", function(passwords) {
+    console.log(passwords.length);
+    for(var i=0; i<passwords.length; i++){
+        if(valorABuscar.localeCompare(passwords[i].plataforma.toLowerCase())==0){
+        var lastString= "<td>"+ passwords[i].plataforma + "</td>" + "<td>"+ passwords[i].cuenta + "</td>" + "<td>"+ passwords[i].password + "</td>";
+        $('#passwords').html(lastString);
+      }
+    }
+  });
+});
+}
+
+$(function(){
+  $("#agregar").on("click", function(){
+    $("#formAgregarPass").validate ({
+      rules:{
+        plataforma:{required:true,maxlength:21},
+        cuenta:{required:true,maxlength:58},
+        password:{required:true,maxlength:39},
+      },
+      messages:{
+        plataforma:{required:"Campo Obligatorio",maxlength:"Maximo 21"},
+        cuenta:{required:"Campo Obligatorio",maxlength:"Maximo 58"},
+        password:{required:"Campo Obligatorio",maxlength:"Maximo 39"},
+      }
     });
   });
 });
